a5f6ebdcee50546b8773748865386677dbfb6372
ak <479432115@qq.com>
2021-09-03 18:00:42 +0800

    add picture resource

--- app/src/main/java/com/example/recordjournal/MainActivity.java
+++ app/src/main/java/com/example/recordjournal/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * ak
+ * name:简约记账
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
     }
 }
